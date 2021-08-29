3583e1ee97789c6f90a24824776dde1ca7dd3ea8
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-08-29 16:58:11 -0300

    my first time with react native

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  Animated,
+} from "react-native";
+import { server as DATA } from "./server";
+
+const { width } = Dimensions.get("screen");
+const imageW = width * 0.9;
+const imageH = imageW * 1.54;
 
 export default function App() {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      <View style={styles.corouselBackground}>
+        {DATA.map(({ image }, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, 1, 0],
+          });
+          return (
+            <Animated.Image
+              key={`image-${index}`}
+              source={{ uri: image }}
+              style={[StyleSheet.absoluteFillObject, { opacity }]}
+              blurRadius={50}
+            />
+          );
+        })}
+      </View>
+      <Animated.FlatList
+        data={DATA}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(_, index) => index.toString()}
+        horizontal
+        pagingEnabled
+        renderItem={({ item: { image, title, text } }) => {
+          return (
+            <View style={styles.imageContainer}>
+              <Image source={{ uri: image }} style={styles.image} />
+              <Text>{title}</Text>
+              <Text>{text}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +67,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+  },
+  corouselBackground: StyleSheet.absoluteFillObject,
+  imageContainer: { width, justifyContent: "center", alignItems: "center" },
+  image: {
+    width: imageW,
+    height: imageH,
+    resizeMode: "cover",
   },
 });
---
+++ server.js
@@ -0,0 +1,25 @@
+export const server = [
+  {
+    title: "monogatari series",
+    image: "https://i.redd.it/dm8m3yz2tof01.jpg",
+    text: "The story centers on Koyomi Araragi, a 3rd-year high school student who finds himself mixed up with all kinds of ghosts, demons, apparitions, and cute girls!",
+  },
+  {
+    title: "steins; Gate",
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/648/710/568/portrait-display-anime-steins-gate-makise-kurisu-rintaro-okabe-hd-wallpaper-preview.jpg",
+    text: "Steins;Gate, Makise Kurisu, Rintaro Okabe",
+  },
+  {
+    title: "ore wo suki nano wa omae dake ka yo Pansy",
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5962a059-cb54-4ba1-8917-84a5b1e5fef6/ddn0btk-4b128673-6b06-4ce6-803f-af8288213788.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU5NjJhMDU5LWNiNTQtNGJhMS04OTE3LTg0YTViMWU1ZmVmNlwvZGRuMGJ0ay00YjEyODY3My02YjA2LTRjZTYtODAzZi1hZjgyODgyMTM3ODguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ZpDj_6K0KMXC3bfVlwkZW9Qd68FPx446xQbCrsNthAU",
+    text: "random anime",
+  },
+  {
+    title: "kiss shot",
+    image:
+      "https://cdna.artstation.com/p/assets/images/images/028/885/242/large/drid-dass-1594886132586.jpg?1595825871",
+    text: "monogatari series",
+  },
+];
